c9a3ced1d1d43370bc6cd05d01955f97a991dc35
sosongha3 <sosongha3@ajou.ac.kr>
2025-04-10 17:40:24 +0900

    💄 Add LoginScreen with new components

--- app/src/main/java/org/sopt/at/feature/onboarding/LoginScreen.kt
+++ app/src/main/java/org/sopt/at/feature/onboarding/LoginScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -21,9 +22,12 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
@@ -42,6 +46,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
+import org.sopt.at.ui.component.BasicButton
+import org.sopt.at.ui.component.LoginTextField
 import org.sopt.at.ui.component.ReturnBar
 import org.sopt.at.ui.theme.Gray0
 import org.sopt.at.ui.theme.Gray20
@@ -51,131 +57,83 @@ import org.sopt.at.ui.theme.Gray60
 @Preview
 @Composable
 fun LoginSCreen(
+    idValue: String,
+    pwdValue: String,
+    onIdValueChange: (String) -> Unit,
+    onPwdValueChange: (String) -> Unit,
     onReturnClicked: () -> Unit,
-    onLoginClicked: () -> Unit
+    onLoginClicked: () -> Unit,
+    onFindIdClicked: () -> Unit,
+    onFindPwdClicked: () -> Unit,
+    onSignupClicked: () -> Unit,
+    title: String,
 ) {
-    val context : Context = LocalContext.current
-    var textId by remember { mutableStateOf("") }
-    var textPwd by remember { mutableStateOf("") }
-    val scope = rememberCoroutineScope()
-    val snackbarHostState = remember { SnackbarHostState() }
-    var snackbarHost = SnackbarHost(hostState = snackbarHostState)
 
-    Column(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.Start,
-        verticalArrangement = Arrangement.Top
-    ){
-        ReturnBar(onReturnClicked)
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    )
+    {
         Column(
-            modifier = Modifier.padding(20.dp).fillMaxHeight(),
-            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+            horizontalAlignment = Alignment.Start,
             verticalArrangement = Arrangement.Top
-        ) {
+        ){
+            ReturnBar(onReturnClicked)
             Column(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.Start,
-                verticalArrangement = Arrangement.SpaceBetween
-            ){
-                Spacer(modifier = Modifier.fillMaxWidth().height(20.dp))
-                Text(text = "TVING ID 로그인",
-                    modifier = Modifier,
-                    fontSize = 25.sp,
-                    color = Gray0)
-                Spacer(modifier = Modifier.fillMaxWidth().height(30.dp))
-            }
-
-            Column (
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = BiasAlignment.Horizontal(-1f),
-                verticalArrangement = Arrangement.SpaceBetween
-            ){
-                TextField(
-                    value = textId,
-                    onValueChange = {value ->
-                        textId = value},
-                    modifier = Modifier.fillMaxWidth().padding(top = 5.dp, bottom = 5.dp),
-                    placeholder = { Text(text ="아이디", color = Gray20) },
-                    singleLine = true,
-                    shape = RoundedCornerShape(8.dp),
-                    colors = TextFieldDefaults.colors(
-                        focusedContainerColor = Gray60,
-                        unfocusedContainerColor = Gray60,
-                        disabledContainerColor = Gray60,
-                        errorContainerColor = Gray60,
-                        focusedIndicatorColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.Transparent,
-                        disabledIndicatorColor = Color.Transparent,
-                        errorIndicatorColor = Color.Transparent
-                    )
-                )
-                Box(
-                    modifier = Modifier.fillMaxWidth().padding(top = 5.dp, bottom = 5.dp)
-                )
-                {
-                    TextField(
-                        value = textPwd,
-                        onValueChange = {value -> textPwd = value},
-                        modifier = Modifier.fillMaxWidth(),
-                        placeholder = { Text(text ="비밀번호", color = Gray20) },
-                        singleLine = true,
-                        shape = RoundedCornerShape(8.dp),
-                        colors = TextFieldDefaults.colors(
-                            focusedContainerColor = Gray60,
-                            unfocusedContainerColor = Gray60,
-                            disabledContainerColor = Gray60,
-                            errorContainerColor = Gray60,
-                            focusedIndicatorColor = Color.Transparent,
-                            unfocusedIndicatorColor = Color.Transparent,
-                            disabledIndicatorColor = Color.Transparent,
-                            errorIndicatorColor = Color.Transparent
-                        )
-                    )
-                    IconButton(
-                        onClick = {},
-                        modifier = Modifier.align(Alignment.CenterEnd),
-                        enabled = true
-                    ) {
-                        Icon(
-                            modifier = Modifier,
-                            imageVector = Icons.Filled.VisibilityOff,
-                            tint = Gray20,
-                            contentDescription = null
-                        )
-                    }
+                modifier = Modifier.padding(20.dp).fillMaxHeight(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.Start,
+                    verticalArrangement = Arrangement.SpaceBetween
+                ){
+                    Spacer(modifier = Modifier.fillMaxWidth().height(20.dp))
+                    Text(text = title,
+                        modifier = Modifier,
+                        fontSize = 25.sp,
+                        color = Gray0)
+                    Spacer(modifier = Modifier.fillMaxWidth().height(30.dp))
                 }
 
+                Column (
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = BiasAlignment.Horizontal(-1f),
+                    verticalArrangement = Arrangement.SpaceBetween
+                ){
+                    LoginTextField(idValue, onIdValueChange, "아이디", false)
+                    LoginTextField(pwdValue, onPwdValueChange, "비밀번호", true)
+                }
+                BasicButton("로그인하기", onLoginClicked)
+                Row(
+                    modifier = Modifier.height(30.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceAround
+                )
+                {
+                    TextButton(
+                        onClick = onFindIdClicked
+                    ) {
+                        Text("아이디 찾기", fontSize = 15.sp, textAlign = TextAlign.Center, color = Gray20)
+                    }
+                    Spacer(modifier = Modifier.padding(7.dp).width(1.dp).fillMaxHeight().background(Gray20))
+                    TextButton(
+                        onClick = onFindPwdClicked
+                    ) {
+                        Text("비밀번호 찾기", fontSize = 15.sp, textAlign = TextAlign.Center, color = Gray20)
+                    }
+                    Spacer(modifier = Modifier.padding(7.dp).width(1.dp).fillMaxHeight().background(Gray20))
+                    TextButton(
+                        onClick = onSignupClicked
+                    ) {
+                        Text("회원가입", fontSize = 15.sp, textAlign = TextAlign.Center, color = Gray20)
+                    }
+                }
+                Spacer(modifier = Modifier.height(15.dp).fillMaxWidth())
+                Text("이 사이트는 Google reCAPTCHA로 보호되며, \nGoogle 개인정보 처리방침과 서비스 약관이 적용됩니다.", fontSize = 12.sp, textAlign = TextAlign.Center, color = Gray20)
             }
-            Button(
-                onClick = onLoginClicked,
-                modifier = Modifier.fillMaxWidth().padding(top = 20.dp, bottom = 20.dp).height(50.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Gray40),
-                shape = RoundedCornerShape(5.dp)
-            ) {
-                Text("로그인하기", fontSize = 17.sp, color = Gray20)
-            }
-            Row(
-                modifier = Modifier.height(30.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceAround
-            )
-            {
-                Text("아이디 찾기", fontSize = 15.sp, textAlign = TextAlign.Center, color = Gray20)
-                Spacer(modifier = Modifier.padding(7.dp)
-                    .width(1.dp)
-                    .fillMaxHeight()
-                    .background(Gray20))
-                Text("비밀번호 찾기", fontSize = 15.sp, textAlign = TextAlign.Center, color = Gray20)
-                Spacer(modifier = Modifier.padding(7.dp)
-                    .width(1.dp)
-                    .fillMaxHeight()
-                    .background(Gray20))
-                Text("회원가입", fontSize = 15.sp, textAlign = TextAlign.Center, color = Gray20)
-            }
-            Spacer(modifier = Modifier
-                .height(15.dp)
-                .fillMaxWidth())
-            Text("이 사이트는 Google reCAPTCHA로 보호되며, \nGoogle 개인정보 처리방침과 서비스 약관이 적용됩니다.", fontSize = 12.sp, textAlign = TextAlign.Center, color = Gray20)
         }
     }
 }
---
+++ app/src/main/java/org/sopt/at/ui/component/BasicButton.kt
@@ -0,0 +1,42 @@
+package org.sopt.at.ui.component
+
+import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
+import org.sopt.at.ui.theme.Gray0
+import org.sopt.at.ui.theme.Gray20
+import org.sopt.at.ui.theme.Gray40
+
+@Composable
+fun BasicButton(
+    text: String,
+    onClick: () -> Unit
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    Button(
+        onClick = onClick,
+        modifier = Modifier.fillMaxWidth().height(50.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = Gray40, contentColor = Gray0),
+        shape = RoundedCornerShape(5.dp),
+        interactionSource = interactionSource
+    ) {
+        Text(text, fontSize = 17.sp, color = Gray20)
+    }
+}
