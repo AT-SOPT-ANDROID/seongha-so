13f6d9647136b304e0eae76341eb7b55b9c78eea
sosongha3 <sosongha3@ajou.ac.kr>
2025-04-17 19:45:56 +0900

    ➕ Add Coil dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.material.icons.core)
     implementation(libs.androidx.material.icons.extended)
     implementation(libs.accompanist.systemuicontroller)
+    implementation(libs.io.coil)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
