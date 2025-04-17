8e54e7a44634b99eae9f8964a72c8aee2e7cfeee
sosongha3 <sosongha3@ajou.ac.kr>
2025-04-18 01:54:26 +0900

    🍱 Add example content data for HomeTab

--- app/src/main/java/org/sopt/at/feature/home/HomeScreen.kt
+++ app/src/main/java/org/sopt/at/feature/home/HomeScreen.kt
@@ -49,6 +49,7 @@ import org.sopt.at.core.tab.HomeTabType
 import org.sopt.at.core.tab.TabType
 import org.sopt.at.domain.entity.Content
 import org.sopt.at.feature.mypage.MyPage
+import org.sopt.at.feature.onboarding.SignUpStep
 import org.sopt.at.ui.component.AtSoptContentRow
 import org.sopt.at.ui.component.AtSoptContentViewPager
 import org.sopt.at.ui.component.AtSoptTabBar
@@ -66,40 +67,15 @@ fun Preview(){
 fun HomeScreen(
     entireNavController: NavHostController,
 ) {
-    //TODO 임시 데이터
-    val contents = listOf<Content>(
-        Content(
-            rank = 1,
-            title = "광해",
-            image = "https://i.namu.wiki/i/TqVoGq4Cs6XvdgZ5VbxTZPxBtVoksQC1ZWS8jJzrsCOFaFq88EmCF1ndlzn6OtCagRkUYv7E7ayjUzc2RMtXhA.webp",
-            description = "왕이 된 남자"
-        ),
-        Content(
-            rank = 2,
-            title = "뽀로로",
-            image = "https://i.namu.wiki/i/uxxXRrUEW7Nud72yH2YKsciBZGJD-XeJdkYSg5Jpp97kOwq5gESAuAHYeaqV9BJpzvHRaMOrDbEoN9U2B9RhZQ.webp",
-            description = "뽀로로와 올 겨울을 함께 해요!"
-        ),
-        Content(
-            rank = 3,
-            title = "여신강림",
-            image = "https://upload.wikimedia.org/wikipedia/ko/d/d7/%EC%97%AC%EC%8B%A0%EA%B0%95%EB%A6%BC_%EB%93%9C%EB%9D%BC%EB%A7%88_%ED%8F%AC%EC%8A%A4%ED%84%B0.jpg",
-            description = "메이크업으로 여신이 된 주경이는 꿈과 사랑을 이룰 수 있을까?"
-        ),
-        Content(
-            rank = 4,
-            title = "신서유기",
-            image = "https://i.namu.wiki/i/0-RXBLtGoNqcyV9TEiLD-ftdqS42a9jc5r29EsDcqMGENwCpFShtSfHfjxx4jliGnuy-wtnKZPtx2mCpi-lXaw.webp",
-            description = "메이크업으로 여신이 된 주경이는 꿈과 사랑을 이룰 수 있을까?"
-        ),
-        Content(
-            rank = 5,
-            title = "사카모토입니다만?",
-            image = "https://i.namu.wiki/i/iDjj7hRfDbp78k0XYmkqWtg5Hq5aa_uF0LTk3SiWzoZ5Rux01keM6VOtcD3QpIjH8KkLdhygVDejl54gnhkm4Q.webp",
-            description = "Cool, Cooler, Coolest 고등학생 등장!"
-        ),
-    )
     var selectedTab by remember { mutableStateOf(HomeTabType.DRAMA) }
+    val contents = when(selectedTab){
+        HomeTabType.DRAMA -> ExampleContent.dramaContents
+        HomeTabType.ENTERTAINMENT -> ExampleContent.entertainmentContents
+        HomeTabType.MOVIE -> ExampleContent.movieContents
+        HomeTabType.SPORTS -> ExampleContent.sportsContents
+        HomeTabType.ANIMATION -> ExampleContent.animationContents
+        HomeTabType.NEWS -> ExampleContent.newsContents
+    }
     val pagerState = rememberPagerState { contents.size }
 
     LazyColumn(
