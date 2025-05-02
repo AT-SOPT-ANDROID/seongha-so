2759afa09b383f3abc85cbf679d894e7ea052b4d
sosongha3 <sosongha3@ajou.ac.kr>
2025-05-03 00:45:57 +0900

    ⚡️ Improve performance with not using Painter

--- app/src/main/java/org/sopt/at/feature/home/HomeScreen.kt
+++ app/src/main/java/org/sopt/at/feature/home/HomeScreen.kt
@@ -26,8 +26,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -153,7 +155,7 @@ private fun MainStickyHeader(
                 ) {
                     Image(
                         modifier = Modifier.fillMaxSize(),
-                        painter = painterResource(id = R.drawable.icon_tving),
+                        imageVector = ImageVector.vectorResource(id = R.drawable.icon_tving),
                         contentDescription = "mypage button",
                         contentScale = ContentScale.Fit,
                     )
--- app/src/main/java/org/sopt/at/feature/mypage/MyScreen.kt
+++ app/src/main/java/org/sopt/at/feature/mypage/MyScreen.kt
@@ -31,9 +31,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -116,7 +118,7 @@ fun MyScreen(
                                 enabled = true
                             ) {
                                 Image(
-                                    painter = painterResource(id = R.drawable.icon_tving),
+                                    imageVector = ImageVector.vectorResource(id = R.drawable.icon_tving),
                                     contentDescription = null,
                                     contentScale = ContentScale.Fit,
                                     modifier = Modifier.fillMaxSize()
